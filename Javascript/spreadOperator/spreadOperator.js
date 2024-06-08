06779fe8c5e856fda7a9dbfd417f16665cef00dd
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-06-08 13:37:14 +0700

    spread operator

---
+++ Javascript/spreadOperator/spreadOperator.js
@@ -0,0 +1,12 @@
+const hobbys = ["Reading", "Coding", "Gaming"];
+
+const newHobbys = [...hobbys, "Travelling"];
+// console.log(newHobbys);
+
+let hobbys1 = ["Reading", "Coding", "Gaming"];
+
+let hobby2 = ["riding", "swimming"];
+
+let dataHobby = [...hobbys1, "baking", ...hobby2];
+
+console.log(dataHobby);
