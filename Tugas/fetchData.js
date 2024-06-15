9261bdeab4488254659fc82ee409dcc3169d0443
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-06-16 01:19:41 +0700

    Tugas 2

--- Tugas/fetchData.js
+++ Tugas/fetchData.js
@@ -1,4 +1,3 @@
-// Menggunakan async/await
 const fetchUserNames = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -6,32 +5,12 @@ const fetchUserNames = async () => {
       throw new Error("Network response was not ok");
     }
     const users = await response.json();
-    const names = users.map((user) => user.name);
-    console.log(names);
+    users.forEach((user) => {
+      console.log(user.name);
+    });
   } catch (error) {
     console.error("Error fetching user names:", error);
   }
 };
 
 fetchUserNames();
-
-// Menggunakan then/catch
-
-const fetchUsersNames = () => {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((users) => {
-      const names = users.map((user) => user.name);
-      console.log(names);
-    })
-    .catch((error) => {
-      console.error("Error fetching user names:", error);
-    });
-};
-
-fetchUsersNames();
